b3626ddba3db0d7ed8b6b89646174dff0a6a2270
AlexB135 <80590603+AlexB135@users.noreply.github.com>
2021-03-13 17:39:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml"&gt;
+<head>
+<meta charset="utf-8" />
+<title></title>
+</head>
+<body>
+
+<form method="post" action="pr.php" >
+<label> Введите Ваше имя </label> <input type="text" name="t1"/><br>
+<label> Пароль </label> <input type="password" name="t2"/><br>
+
+<label> Сообщение </label> <input type="text" name="t3"/><br>
+<input type="submit" value="Отправить"/><br>
+</form>
+
+</body>
+</html>
